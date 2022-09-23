9ed7a1afe49a11641f6743b035c31b695a98630a
Ysquid <ahk648@gmail.com>
2022-09-22 19:06:52 -0600

    Built MVP function

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 function uncleJoey() {
-    return console.log('Hey it\'s Uncle Joey Cocksuckas!')
+    
+    //**Lines for the quote blanks**//
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    const setups = [
+    'Powder up those nuts',
+    'Shine your fucking shoes',
+    'Take that morning rip',
+    'Get your morning coffee in',
+    'Buckle your fucking chin strap',
+    'Look this day right in it\'s fucking eyes',
+    'Put yesterday\'s bullshit behind you'
+    ]
+    const tags = [
+        'give it your best go - what do ya got to lose',
+    'rock and fucking roll cocksucka',
+    'let the haters know who you are',
+    'fingerblast this day to the grown',
+    'rip the tits off today',
+    'crush your goals like a Dodge Ram crushes turtles',
+    'rip it harder than OJ in a ford Bronco'
+    ]
+
+    const day = days[(Math.floor(Math.random() * 7))]
+    const setup = setups[(Math.floor(Math.random() * 7))]
+    const tag = tags[(Math.floor(Math.random() * 7))]
+
+    
+
+    return `It\'s ${day} cocksucka! It's time to ${setup} and ${tag}`
 }
 
-uncleJoey()
+console.log(uncleJoey())
